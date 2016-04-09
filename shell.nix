a15048d11ef6014ac46c79cdf74e25b90dece994
adamConnerSax <adam_conner_sax@yahoo.com>
2016-04-08 21:50:41 -0400

    simplified webkit/stack work

--- shell.nix
+++ shell.nix
@@ -2,7 +2,9 @@ with (import <nixpkgs> {});
 
 haskell.lib.buildStackProject {
   name = "myEnv";
-  buildInputs = [ freetype webkitgtk24x darwin.security_tool gcc git fontconfig ];
+  buildInputs = [ webkitgtk24x darwin.security_tool gcc49 git ];
+  GIT_CURL_VERBOSE=1;
+  GIT_SSL_CAINFO="${cacert}/etc/ssl/certs/ca-bundle.crt";
   SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt";
   PANGOCAIRO_BACKEND="";
   FONTCONFIG_FILE="${fontconfig}/etc/fonts/fonts.conf";
