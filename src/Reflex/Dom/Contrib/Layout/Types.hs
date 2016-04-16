bfd729b70bc78eba7837853a4461e1396baf7093
adamConnerSax <adam_conner_sax@yahoo.com>
2016-04-16 14:42:33 -0400

    working...

--- src/Reflex/Dom/Contrib/Layout/Types.hs
+++ src/Reflex/Dom/Contrib/Layout/Types.hs
@@ -3,16 +3,21 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
 module Reflex.Dom.Contrib.Layout.Types where
 
 import Control.Lens (makeLenses,makeClassy)
+import qualified Control.Category as C
 import qualified Reflex as R
 import Reflex.Dom ((=:))
+import qualified Reflex.Dom as RD 
 import qualified Data.Map as M
 import qualified GHCJS.DOM.Element as E
 import Control.Monad.State (StateT)
 import Control.Monad.Reader (ReaderT)
-
+import Data.Monoid ((<>))
 --import Data.Default (Default(..))
 
 class IsCssClass a where
@@ -35,6 +40,59 @@ instance Monoid CssClasses where
 
 emptyCss::CssClasses
 emptyCss = CssClasses []
+
+{-
+data StyleAdder a b = StyleAdder { saClasses::CssClasses, saAdder::CssClasses->a->b } 
+
+instance Category StyleAdder where
+  id = StyleAdder emptyCss (const id)
+  (StyleAdder cs1 add1) . (StyleAdder cs2 add2) = StyleAdder (cs1 <> cs2) ( 
+-}
+
+class AddStyle a where
+  addStyle::CssClasses->a->a
+
+instance AddStyle CssClasses where
+  addStyle cs1 cs2 = cs1 <> cs2
+
+instance RD.MonadWidget t m=>AddStyle (m a) where
+  addStyle cs ma = RD.divClass (toCssString cs) ma
+  {-# NOINLINE addStyle #-}
+
+{-# RULES "addStyle/addStyle"  forall (x :: CssClasses) (y :: CssClasses) z . addStyle x (addStyle y z) = addStyle (x <> y) z   #-}
+ 
+{-
+instance (AddStyle b c, RD.MonadWidget t m)=>AddStyle b (m a) where
+  addStyle as ma = \as -> f (addStyle cs as)
+
+instance Category 
+-}
+
+data Nester m a b where
+  Nest::(m a -> m b) -> Nester m a b
+  Layout::(forall a.m a -> m a) -> Nester m b b
+
+-- this forms a category, right?
+emptyNester::Nester m a a
+emptyNester = Layout id
+
+composeNesters::Nester m b c->Nester m a b->Nester m a c
+composeNesters (Layout l1) (Layout l2) = Layout (l1 . l2)
+composeNesters (Layout l) (Nest n) = Nest (l . n)
+composeNesters (Nest n) (Layout l) = Nest (n . l)
+composeNesters (Nest n1) (Nest n2) = Nest (n1 . n2)
+
+infixl 6 ##
+(##)::Nester m b c->Nester m a b->Nester m a c
+(##) = composeNesters
+
+instance C.Category (Nester m) where
+  id = emptyNester
+  (.) = composeNesters
+  
+applyNester::Nester m a b -> m a -> m b
+applyNester (Nest n) action = n  action
+applyNester (Layout l) action = l  action
 
 
 type CssGridRowClass = CssClass
