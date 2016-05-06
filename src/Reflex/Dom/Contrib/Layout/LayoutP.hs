032c8ba1c6d25ca875b9b858a86078dfe5eed033
adamConnerSax <adam_conner_sax@yahoo.com>
2016-05-06 17:41:13 -0400

    working...

--- src/Reflex/Dom/Contrib/Layout/LayoutP.hs
+++ src/Reflex/Dom/Contrib/Layout/LayoutP.hs
@@ -10,11 +10,16 @@ module Reflex.Dom.Contrib.Layout.LayoutP where
 
 import qualified Reflex as R
 import qualified Reflex.Class as RC
+import qualified Reflex.Host.Class as RHC
 import qualified Reflex.Dom as RD
 
-import Control.Monad.State (StateT,runStateT,lift,get,put)
-import Control.Monad.Trans (MonadIO)
-import Control.Monad.Ref (MonadRef)
+import Control.Monad.Fix (MonadFix(mfix))
+import Control.Monad.State (StateT,runStateT,execStateT,lift,get,put)
+import Control.Monad.Trans (MonadIO,MonadTrans)
+import Control.Monad.Ref (MonadRef,Ref)
+import Control.Monad.Morph
+import System.IO
+
 
 import qualified Reflex.Dom.Contrib.Layout.Types as LT
 
@@ -24,12 +29,22 @@ data LayoutInstruction = LayoutInstruction LayoutConstraint LayoutNodeType LT.Cs
 
 newtype LPS = LPS [LayoutInstruction]
 
-newtype LayoutP t m a = LayoutP (StateT LPS m a)
+newtype LayoutP t m a = LayoutP {  unLayoutP::StateT LPS m a }
 
 instance Functor m=>Functor (LayoutP t m)
 instance Applicative m=>Applicative (LayoutP t m)
 instance Monad m=>Monad (LayoutP t m)
+
+instance MonadTrans (LayoutP t) where
+  lift m = LayoutP $ lift m
+
 instance MonadRef m=>MonadRef (LayoutP t m)
+
+instance MFunctor (LayoutP t) where
+  hoist nat (LayoutP sma) = LayoutP $ hoist nat sma
+
+instance MonadFix m=>MonadFix (LayoutP t m) where
+  mfix f = LayoutP $ mfix (unLayoutP . f) 
 
 instance RC.MonadSample t m => RC.MonadSample t (LayoutP t m) where
   sample = LayoutP . lift . RC.sample
@@ -37,52 +52,43 @@ instance RC.MonadSample t m => RC.MonadSample t (LayoutP t m) where
 instance RC.MonadHold t m => RC.MonadHold t (LayoutP t m) where
   hold a0 = LayoutP . lift . RC.hold a0 
 
+instance RD.HasWebView m => RD.HasWebView (LayoutP t m) where
+  askWebView = LayoutP RD.askWebView
+
+instance RD.HasDocument m => RD.HasDocument (LayoutP t m) where
+  askDocument = LayoutP RD.askDocument
+
+instance RHC.MonadReflexCreateTrigger t m => RHC.MonadReflexCreateTrigger t (LayoutP t m) where
+  newEventWithTrigger = LayoutP . RHC.newEventWithTrigger
+  newFanEventWithTrigger = LayoutP . RHC.newFanEventWithTrigger
+
+{-
 instance (RD.MonadWidget t m,RD.MonadIORestore m, MonadIO (RD.PushM t)) => RD.MonadIORestore (LayoutP t m) where
   askRestore = LayoutP $ do
     parentRestore <- lift RD.askRestore
     curState <- get
-    return $ RD.Restore $ \(LayoutP sma) -> RD.restore parentRestore $ fst <$> runStateT sma curState
+    return $ RD.Restore $ \(LayoutP sma) -> RD.restore parentRestore $ execStateT sma curState
+-}
 
-
-instance RD.HasPostGui t h m => RD.HasPostGui t h (LayoutP t m) where
+instance (RD.HasPostGui t h m, Ref (LayoutP t m) ~ Ref h) => RD.HasPostGui t h (LayoutP t m) where
   askPostGui = LayoutP $ lift RD.askPostGui
   askRunWithActions = LayoutP $ lift RD.askRunWithActions
 
-{-
--- which to use??  Both work on demo.  So far.  Need to add widgetHold or dyn...
-layoutInside::(MonadIO (RD.PushM t),RD.MonadWidget t m)=>(m a -> m b)->LayoutM t m a->LayoutM t m b
-layoutInside f w = do
-  lc <- ask
-  dynamicCssMap <- use lsDynamicCssMap
-  staticCssMap <- use lsClassMap
-  liftL $ f $ runLayout staticCssMap dynamicCssMap lc w
 
-noLayoutInside::(RD.MonadWidget t m ,s~LayoutS t)=>(d->(b,s))->(m (a,s) -> m d)->LayoutM t m a->LayoutM t m b
-noLayoutInside f action w = do
-  s <- get
-  lc <- ask
-  (x,s') <- f <$> (liftL $ action $ runReaderT (runStateT w s) lc)
-  put s'
-  return x
-
-instance (RD.MonadWidget t m,MonadIO (RD.PushM t))=>RD.MonadWidget t (LayoutM t m) where
-  type WidgetHost (LayoutM t m) = RD.WidgetHost m
-  type GuiAction  (LayoutM t m) = RD.GuiAction m
-  askParent = liftL RD.askParent
---  subWidget n w = noLayoutInside id (RD.subWidget n) w
---  subWidgetWithVoidActions n w = noLayoutInside (\((a,s),ev)->((a,ev),s)) (RD.subWidgetWithVoidActions n)  w
-  subWidget n w = layoutInside (RD.subWidget n) w
-  subWidgetWithVoidActions n w = layoutInside (RD.subWidgetWithVoidActions n)  w
-  liftWidgetHost = liftL . RD.liftWidgetHost
-  schedulePostBuild = liftL . RD.schedulePostBuild
-  addVoidAction = liftL . RD.addVoidAction
+instance (RD.MonadWidget t m, MonadIO (RD.PushM t), Ref (LayoutP t m) ~ Ref IO)=>RD.MonadWidget t (LayoutP t m) where
+  type WidgetHost (LayoutP t m) = RD.WidgetHost m
+  type GuiAction  (LayoutP t m) = RD.GuiAction m
+  askParent = lift RD.askParent
+  subWidget n w = hoist (RD.subWidget n) w
+  subWidgetWithVoidActions n w = hoist (RD.subWidgetWithVoidActions n)  w
+  liftWidgetHost = lift . RD.liftWidgetHost
+  schedulePostBuild = lift . RD.schedulePostBuild
+  addVoidAction = lift . RD.addVoidAction
 
 -- What happens if w changes the state??
   getRunWidget = do
-    lc <- ask
-    dynamicCssMap <- use lsDynamicCssMap
-    staticCssMap <- use lsClassMap
-    runWidget <- liftL RD.getRunWidget
-    return $ \rootElement w -> runWidget rootElement $ runLayout staticCssMap dynamicCssMap lc w
+    curS <- get
+    runWidget <- lift RD.getRunWidget
+    return $ \rootElement w -> runWidget rootElement $ execStateT w curS
 
--}
+
