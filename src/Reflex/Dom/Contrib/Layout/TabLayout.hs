92d99d621d57624714e926c5104899d448ec0e47
adamConnerSax <adam_conner_sax@yahoo.com>
2017-02-05 11:39:24 -0500

    Fixed tab layout.  Now trying to add style for checked label only.

--- src/Reflex/Dom/Contrib/Layout/TabLayout.hs
+++ src/Reflex/Dom/Contrib/Layout/TabLayout.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE RecursiveDo           #-}
 module Reflex.Dom.Contrib.Layout.TabLayout
        (
          tabCssBS
@@ -15,13 +16,18 @@ module Reflex.Dom.Contrib.Layout.TabLayout
 
 
 import Reflex.Dom.Contrib.Layout.ClayUtils
-
+import Reflex.Dom.Contrib.Layout.FlexLayout
 import qualified Reflex as R
+import Reflex.Dynamic ()
 import qualified Reflex.Dom as RD
+
 import Reflex.Dom.Contrib.Widgets.DynTabs
+import Control.Monad.Fix (MonadFix)
 import Control.Monad.IO.Class (MonadIO)
 import qualified Data.ByteString as B
 import qualified Data.Map as M
+import qualified Data.Foldable as F
+
 
 import Prelude hiding (div)
 import Clay
@@ -49,6 +55,10 @@ tabCss = do
       cursor pointer
       zIndex 1
       marginLeft (px (-1))
+      ".selected" ? do
+        background (grayish 200)
+      ".unselected" ?
+        background white
     label # firstOfType ? do
       marginLeft (px 0)
     ".tab-div" ? do
@@ -61,6 +71,11 @@ tabCss = do
     input # ("type" @= "radio") ? display none
     input # ("type" @= "radio") # checked |+ label |+ div ? do
       display block
+  ".tab-div2" ? do
+    width (pct 100)
+    marginTop (px (-1))
+    sym padding (em 1)
+    border solid (px 1) (grayish 211)
   ".dyn-tab-bar" ? do
     ul ? do
       listStyle none none none
@@ -87,20 +102,41 @@ data TabInfo t m a = TabInfo { tabID::T.Text,
                                tabName::T.Text,
                                tabWidget::(RD.DomBuilder t m,MonadIO (R.PushM t))=>m a }
 
-staticTabbedLayout::(MonadIO (R.PushM t),RD.DomBuilder t m,Traversable f)=>TabInfo t m a->f (TabInfo t m a)->m (f a)
-staticTabbedLayout curTab tabs = RD.divClass "tabbed-area" $ do
+
+attrsIf::R.Reflex t=>M.Map T.Text T.Text->M.Map T.Text T.Text -> M.Map T.Text T.Text -> R.Dynamic t Bool -> R.Dynamic t (M.Map T.Text T.Text)
+attrsIf alwaysAttrs trueAttrs falseAttrs ifDyn = f <$> ifDyn where
+  f True = alwaysAttrs <> trueAttrs
+  f False = alwaysAttrs <> falseAttrs
+
+
+staticTabbedLayout'::(MonadIO (R.PushM t),RD.DomBuilder t m,Traversable f)=>TabInfo t m a->f (TabInfo t m a)->m (f a)
+staticTabbedLayout' curTab tabs = RD.divClass "tabbed-area" $ do
   let curTabchecked tab = if tab==curTab then "checked" RD.=: "" else M.empty
-      tabInput t@(TabInfo tabId _ _ )= RD.elAttr "input" (("id" RD.=: tabId)
-                                                        <> ("type" RD.=: "radio")
-                                                        <> ("name" RD.=: "grp")
-                                                        <> curTabchecked t) RD.blank
---      tabLabelClickedEv (TabInfo tabId tabName _) = (RD.domEvent RD.Click . fst) <$> RD.elAttr' "label" ("for" RD.=: tabId) $ RD.text tabName
-      tabLabel (TabInfo tabId tabName _) = RD.elAttr "label" ("for" RD.=: tabId) $ RD.text tabName
+      tabInput tab = RD.elAttr "input" (("id" RD.=: (tabID tab))
+                                         <> ("type" RD.=: "radio")
+                                         <> ("name" RD.=: "grp")
+                                         <> curTabchecked tab) RD.blank
+      tabLabel tab = RD.elAttr "label" ("for" RD.=: (tabID tab)) $ RD.text (tabName tab)
       tabEv ti = tabInput ti >> tabLabel ti
-      contentDiv (TabInfo _ _ w) = RD.divClass "tab-div" w
---  (traverse tabEv tabs) >> traverse contentDiv tabs
+      contentDiv tab = RD.divClass "tab-div" (tabWidget tab)
   traverse (\t -> tabEv t >> contentDiv t) tabs
---  return ()
+
+staticTabbedLayout::(MonadIO (R.PushM t),RD.DomBuilder t m, RD.PostBuild t m, MonadFix m,
+                      RD.MonadHold t m,Traversable f)=>TabInfo t m a->f (TabInfo t m a)->m (f a)
+staticTabbedLayout curTab tabs = RD.divClass "tabbed-area" $ mdo
+  let curTabchecked tab = if tab==curTab then "checked" RD.=: "" else M.empty
+      tabInput tab = RD.elAttr "input" (("id" RD.=: (tabID tab))
+                                         <> ("type" RD.=: "radio")
+                                         <> ("name" RD.=: "grp")
+                                         <> curTabchecked tab) RD.blank -- TabInfo m t () -> m ()
+      makeLabelAttrs tab ct = attrsIf ("for" RD.=: (tabID tab)) ("class" RD.=: "selected") ("class" RD.=: "unselected") ((==tab) <$> ct)
+      tabLabelEv tab ct = fmap (const tab) . (RD.domEvent RD.Click . fst) <$> (RD.elDynAttr' "label" (makeLabelAttrs tab ct) $ RD.text (tabName tab))
+      tabEv tab ct = flexItem (tabInput tab >> tabLabelEv tab ct)
+      makeTabAttrs tab ct = attrsIf ("class" RD.=: "tab-div2") ("style" RD.=: "display: block") ("style" RD.=: "display: none") ((==tab) <$> ct)
+      contentDiv ctDyn tab = RD.elDynAttr "div" (makeTabAttrs tab ctDyn) (tabWidget tab)
+  curTabEv <- RD.leftmost . F.toList <$> flexRow (traverse (`tabEv` curTabEv) tabs) -- Event t (TabInfo t m a)
+  curTabDyn <- R.foldDyn const curTab curTabEv
+  traverse (contentDiv curTabDyn) tabs
 
 
 --dynamic 
