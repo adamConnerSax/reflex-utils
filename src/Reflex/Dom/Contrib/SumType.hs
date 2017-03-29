e7df65c6536a37dc486ccd3913827b40277b0e51
adamConnerSax <adam_conner_sax@yahoo.com>
2017-03-29 10:03:06 -0400

    Cleanup.

--- src/Reflex/Dom/Contrib/SumType.hs
+++ src/Reflex/Dom/Contrib/SumType.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
@@ -15,22 +14,13 @@ module Reflex.Dom.Contrib.SumType where
 
 
 -- FIXME:  make imports specific or qualify them
-import           Generics.SOP            hiding (Compose)
-import           Generics.SOP.Constraint (And, SListIN)
-import           Generics.SOP.NP
-import           Generics.SOP.NS
-import qualified GHC.Generics            as GHC
---import           Data.Type.Equality
---import           Data.Type.Bool
-import           Control.Monad           (join)
-import           Data.Functor.Classes    (Show1, showsPrec1)
-import           Data.Functor.Compose
+import           Control.Monad (join)
+import           Generics.SOP  hiding (Compose)
 import           Reflex
-import           Reflex.Dom
 
 -- utilities
 
-newtype IsCon a = IsCon { unIsCon::Maybe a} deriving (Functor,Applicative,Monad,Show,Show1)
+newtype IsCon a = IsCon { unIsCon::Maybe a} deriving (Functor,Applicative,Monad,Show)
 
 notCon::IsCon a
 notCon = IsCon Nothing
@@ -40,7 +30,7 @@ con = IsCon . Just
 
 instance (Applicative f, Applicative g) => Applicative (f :.: g) where
   pure x= Comp (pure (pure x))
-  fgF <*> fgA = Comp $ ((<*>) <$> (unComp fgF) <*> (unComp fgA))
+  fgF <*> fgA = Comp $ (<*>) <$> unComp fgF <*> unComp fgA
 
 expand::forall (f :: [k] -> *) xs.(SListI xs)=>NS f xs -> NP (IsCon :.: f) xs
 expand ns = go sList (Just ns) where
@@ -48,12 +38,12 @@ expand ns = go sList (Just ns) where
   go SNil _ = Nil
   go SCons mNS = case mNS of
     Nothing -> Comp notCon :* go sList Nothing -- after Z
-    Just ns -> case ns of
+    Just ms -> case ms of
       Z fx -> Comp (con fx) :* go sList Nothing -- at Z
-      S ns' -> Comp notCon :* go sList (Just ns') -- before Z
+      S ms' -> Comp notCon :* go sList (Just ms') -- before Z
 
 
-expandA::Generic a=>a->NP (IsCon :.: (NP I)) (Code a)
+expandA::Generic a=>a->NP (IsCon :.: NP I) (Code a)
 expandA = expand . unSOP . from
 
 type WrappedProjection (g :: * -> *) (f :: k -> *) (xs :: [k]) = K (g (NP f xs)) -.-> g :.: f
@@ -61,7 +51,7 @@ type WrappedProjection (g :: * -> *) (f :: k -> *) (xs :: [k]) = K (g (NP f xs))
 wrappedProjections::forall xs g f.(Functor g,SListI xs) => NP (WrappedProjection g f xs) xs
 wrappedProjections = case sList :: SList xs of
   SNil -> Nil
-  SCons -> fn (Comp . fmap hd . unK) :* liftA_NP shiftWrappedProjection wrappedProjections
+  SCons -> fn (Comp . fmap hd . unK) :* hliftA shiftWrappedProjection wrappedProjections
 
 shiftWrappedProjection :: Functor g=>WrappedProjection g f xs a -> WrappedProjection g f (x ': xs) a
 shiftWrappedProjection (Fn f) = Fn $ f . K . fmap tl . unK
@@ -71,7 +61,7 @@ type WrappedInjection (g :: * -> *) (f :: k -> *) (xs :: [k]) = g :.: f -.-> K (
 wrappedInjections::forall xs g f. (Functor g, SListI xs) => NP (WrappedInjection g f xs) xs
 wrappedInjections = case sList :: SList xs of
   SNil   -> Nil
-  SCons  -> fn (K . fmap Z . unComp) :* liftA_NP shiftWrappedInjection wrappedInjections
+  SCons  -> fn (K . fmap Z . unComp) :* hliftA shiftWrappedInjection wrappedInjections
 
 shiftWrappedInjection:: Functor g=>WrappedInjection g f xs a -> WrappedInjection g f (x ': xs) a
 shiftWrappedInjection (Fn f) = Fn $ K . fmap S . unK . f
@@ -81,9 +71,9 @@ distribute::(Functor h, SListI xs)=>h (NP g xs) -> NP (h :.: g) xs
 distribute x = hap wrappedProjections (hpure $ K x)
 
 distributeI::(Functor h, SListI xs)=>h (NP I xs) -> NP h xs
-distributeI = hliftA (fmap unI . unComp) . distribute
+distributeI = hmap (fmap unI . unComp) . distribute
 
-functorToIsConNP::forall g a.(Functor g,Generic a)=>g a -> NP (g :.: (IsCon :.: (NP I))) (Code a)
+functorToIsConNP::forall g a.(Functor g,Generic a)=>g a -> NP (g :.: (IsCon :.: NP I)) (Code a)
 functorToIsConNP ga = hap wrappedProjections (hpure $ K (expandA <$> ga))
 
 reAssociate::Functor g=>(g :.: (f :.: h)) a -> ((g :.: f) :.: h) a
@@ -92,19 +82,19 @@ reAssociate = Comp . Comp . fmap unComp . unComp
 reAssociateNP::(Functor g, SListI xss)=>NP (g :.: (f :.: h)) xss->NP ((g :.: f) :.: h) xss
 reAssociateNP = hmap reAssociate
 
-isConNPToPOPIsCon::(Functor g, SListI2 xss)=>NP ((g :.: IsCon) :.: (NP I)) xss -> POP (g :.: IsCon) xss
+isConNPToPOPIsCon::(Functor g, SListI2 xss)=>NP ((g :.: IsCon) :.: NP I) xss -> POP (g :.: IsCon) xss
 isConNPToPOPIsCon =
-  let proxyC = Proxy :: Proxy (SListI)
+  let proxyC = Proxy :: Proxy SListI
   in POP . hcliftA proxyC (distributeI . unComp)
 
-reconstructA::(Functor h, Generic a) => NP (h :.: (NP I)) (Code a) -> NP (K (h a)) (Code a)
+reconstructA::(Functor h, Generic a) => NP (h :.: NP I) (Code a) -> NP (K (h a)) (Code a)
 reconstructA = hliftA (K . fmap (to . SOP) . unK) . hap wrappedInjections
 
-type DoAndSequence f h xss = POP f xss -> NP (h :.: (NP I)) xss
+type DoAndSequence f h xss = POP f xss -> NP (h :.: NP I) xss
 
 doAndSequence::(Applicative h, SListI2 xss)=>(forall a.f a->h a) -> DoAndSequence f h xss --POP f xss -> NP (h :.: (NP I)) xss
 doAndSequence q =
-  let sListIC = Proxy :: Proxy (SListI)
+  let sListIC = Proxy :: Proxy SListI
   in hcliftA sListIC (Comp . hsequence) . unPOP . hliftA q
 
 class Map (f :: k -> *) (h :: k -> *) (a :: k) where
@@ -122,7 +112,7 @@ This is the heart of it.  Take a functorial value (Functor g=>g a) and a natural
 a list of [Functor h=>h a], one per constructor of a.
 (g :.: IsCon) is isomorphic to (g :.: Maybe)
 -}
-type TransformEach g h xss = NP ((g :.: IsCon) :.: (NP I)) xss -> NP (h :.: (NP I)) xss
+type TransformEach g h xss = NP ((g :.: IsCon) :.: NP I) xss -> NP (h :.: NP I) xss
 
 functorToPerConstructorList::(Generic a, Functor g, Functor h)=>TransformEach g h (Code a)->g a->[h a]  -- one per constructor
 functorToPerConstructorList transform = hcollapse . reconstructA . transform . reAssociateNP . functorToIsConNP
@@ -152,7 +142,7 @@ dynMaybeIsConToEvent::forall a t (f :: k -> *).Reflex t=>(((Dynamic t :.: Maybe)
 dynMaybeIsConToEvent = Comp . fmapMaybe (join . fmap unIsCon) . updated . unComp . unComp . unComp
 
 whichFired::Reflex t=>[Event t a]->Event t Int
-whichFired = leftmost . zipWith (\n e -> (n <$ e)) [0..]
+whichFired = leftmost . zipWith (<$) [0..]
 
 data ConWidget t m a = ConWidget { conName::ConstructorName, switchedTo::Event t a, widget::(m :.: (Dynamic t :.: Maybe)) a }
 
