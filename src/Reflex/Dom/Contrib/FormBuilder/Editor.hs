312f71eaa34458a5d37496e1375f1f038b8efb60
adamConnerSax <adam_conner_sax@yahoo.com>
2017-06-15 22:55:29 -0400

    Fixing up stack.yaml for jsaddle-snapshot

--- src/Reflex/Dom/Contrib/FormBuilder/Editor.hs
+++ src/Reflex/Dom/Contrib/FormBuilder/Editor.hs
@@ -78,17 +78,6 @@ instance (Reflex t, Monad m, DomBuilder t m, MonadHold t m, PostBuild t m, Combi
   combine :: Compose m h (Compose m h a) -> Compose m h a
   combine x = Compose $ getCompose x >>= getCompose . combine
 
-{-    
--- | This makes any Editor with a (m (DynMaybe t)) result into a Category.
-instance (Reflex t, Monad m, DomBuilder t m, MonadHold t m, PostBuild t m) => Combinable (Compose m (DynMaybe t)) (Compose m (DynMaybe t)) where
-  combine :: Compose m (DynMaybe t) (Compose m (DynMaybe t) a) -> Compose m (DynMaybe t) a
-  combine x = Compose $ getCompose x >>= getCompose . combine
-
--- | This makes any Editor with a (Form t m) result into a Category.
-instance (Reflex t, Monad m, DomBuilder t m, MonadHold t m, PostBuild t m) => Combinable (Form t m) (Form t m) where
-  combine :: Form t m (Form t m a) -> Form t m a
-  combine x = Compose $ getCompose x >>= getCompose . combine
--}
 
 -- | This gives all the powers (Choice, Category, Traversing) to Editors.
 -- but to do so, a dyn happens.  So this is not efficient.  Compositions (categorical or uses of Profunctor Choice, e.g., wander) will need to redraw the widget (when?)
@@ -175,76 +164,18 @@ instance Monad f => Monad (Editor g f a) where
   eb >>= h = Editor $ \ga -> let q = flip runEditor ga in q eb >>= q . h
 
 
-{-
--- | Editor is just a wrapper around the usual form building function.  This wrapper also allows
--- all these instances to be written.
--- Editor is UpStar and DownStar?  There are two functors involved...
-newtype DynEditor t m a b = DynEditor { runDynEditor :: DynMaybe t a -> Form t m b }
+{-    
+-- | This makes any Editor with a (m (DynMaybe t)) result into a Category.
+instance (Reflex t, Monad m, DomBuilder t m, MonadHold t m, PostBuild t m) => Combinable (Compose m (DynMaybe t)) (Compose m (DynMaybe t)) where
+  combine :: Compose m (DynMaybe t) (Compose m (DynMaybe t) a) -> Compose m (DynMaybe t) a
+  combine x = Compose $ getCompose x >>= getCompose . combine
 
-instance (Reflex t, Functor m) => Functor (DynEditor t m a) where
-  fmap f (DynEditor w) = DynEditor $ fmap f . w
-
-instance (Reflex t, Applicative m) => Applicative (DynEditor t m a) where
-  pure x = DynEditor $ const $ pure x
-  (DynEditor wfxy) <*> (DynEditor wx) = DynEditor $ \dma -> wfxy dma <*> wx dma
-
-instance (Reflex t, Functor m) => Profunctor (DynEditor t m) where
-  dimap f g (DynEditor w) = DynEditor $ fmap g . w . fmap f
-
-instance (Reflex t, Applicative m) => Strong (DynEditor t m) where
-  first' (DynEditor w) = DynEditor $ \dmic -> (,) <$> w (fst <$> dmic) <*> unEditedDynMaybe (snd <$> dmic)
-
-{-
--- Seems like this should be possible for a monoidal b, but how do we even specify that here?
-instance (Reflex t, Applicative m) => Choice (FormFrom t m) where
-  left' (FormFrom w) = FormFrom $ \x -> do
-    let fL :: DynMaybe t (Either a c) -> DynMaybe t a
-        fL = Compose .fmap (join . fmap (either Just (const Nothing))). getCompose
-        fR :: DynMaybe t (Either a c) -> DynMaybe t c
-        fR = Compose . fmap (join . fmap (either (const Nothing) Just)). getCompose
-        formL = w $ fL x
-        formR = dynMaybeToForm $ fR x
+-- | This makes any Editor with a (Form t m) result into a Category.
+instance (Reflex t, Monad m, DomBuilder t m, MonadHold t m, PostBuild t m) => Combinable (Form t m) (Form t m) where
+  combine :: Form t m (Form t m a) -> Form t m a
+  combine x = Compose $ getCompose x >>= getCompose . combine
 -}
 
--- | This function just lifts a FormResult directly to a form, without any actual editing
-unEditedFR :: (Reflex t, Applicative m) => FormResult t a -> Form t m a
-unEditedFR = Compose . pure
-
--- | EditorFR is also almost--but for the final composition--Kleisli (FR t m) (FormResult t a) (FormResult t b)
-newtype FREditor t m a b = FREditor { runFREditor :: FormResult t a -> Form t m b }
-
-instance (Reflex t, Functor m) => Functor (FREditor t m a) where
-  fmap f (FREditor w) = FREditor $ fmap f . w
-
-instance (Reflex t, Applicative m) => Applicative (FREditor t m a) where
-  pure x = FREditor $ const $ pure x
-  (FREditor wfxy) <*> (FREditor wx) = FREditor $ \fra -> wfxy fra <*> wx fra
-
-instance (Reflex t, Functor m) => Profunctor (FREditor t m) where
-  dimap f g (FREditor w) = FREditor $ fmap g . w . fmap f
-
-instance (Reflex t, Applicative m) => Strong (FREditor t m) where
-  first' (FREditor w) = FREditor $ \fric -> (,) <$> w (fst <$> fric) <*> unEditedFR (snd <$> fric)
--}
-
-{-
--- liftInput
-newtype Editor (t :: k)  (m :: * -> *) (g :: k -> * -> *) (f :: k -> (* -> *) -> * -> *) (a :: *)  (b :: *) = Editor { runEditor :: g t a -> f t m b }
-
-instance (Reflex t, Functor (f t m)) => Functor (Editor t m g f a) where
-  fmap d (Editor e) = Editor $ fmap d . e
-
-instance (Reflex t, Applicative (f t m)) => Applicative (Editor t m g f a) where
-  pure x = Editor $ const $ pure x
-  (Editor efxy) <*> (Editor ex) = Editor $ \a -> exy a <*> ex a
-
-instance (Reflex t, Functor (g t), Functor (f t m)) => Profunctor (Editor t m g f) where
-  dimap f g (Editor e) = Editor $ fmap g . w . f
-
-instance (Reflex t, Functor (g t), Applicative (f t m)) => Strong (Editor t m) where
-  first' (Editor e) = Editor $ \ic -> (,) <$> e (fst fric) <*> liftInput (snd fric)
-
--}
 
 
 
